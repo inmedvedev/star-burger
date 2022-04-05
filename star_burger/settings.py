f2543e1b6b8c4121d3633775e3f206aedf2f44dc
turim0 <ivan_medvedev_1996@mail.ru>
2022-04-06 02:27:40 +0300

    Add app

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
     'restaurateur.apps.RestaurateurConfig',
+    'geocoder.apps.GeocoderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -28,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'phonenumber_field',
-    'rest_framework',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
